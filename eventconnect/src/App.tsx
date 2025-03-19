179a72e43b57da26524213ea7f169267ef864fbf
FuG1 <a9685679275@gmail.com>
2025-03-19 19:31:35 +0300

    add header on my site

--- eventconnect/src/App.tsx
+++ eventconnect/src/App.tsx
@@ -1,4 +1,4 @@
-import { Header } from './components/header/Header'
+import { Header } from './components/header/Header.tsx'
 import './App.scss'
 
 function App() {
--- eventconnect/src/components/header/Header.tsx
+++ eventconnect/src/components/header/Header.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+// import { Link, useNavigate } from 'react-router-dom'
+import "./header.scss"
 
 export const Header: React.FC = () => {
   return (
-    <div></div>
+    <div className='Header'>
+        <header className='Header__container'>
+            <div className='Header__logo'>
+                <h1>EventConnect</h1>
+            </div>
+            <div className='Header__buttons'>
+                <button className='Header__button__login'>Login</button>
+                <button className='Header__button__signup'>Sign Up</button>
+            </div>
+        </header>
+    </div>
   )
 }
